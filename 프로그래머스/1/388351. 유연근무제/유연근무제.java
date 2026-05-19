d064d2bf613bb551a5b229ecd2aabfffe6d3d72b
김태남 <63178138+kim-tea-nam@users.noreply.github.com>
2026-05-20 00:47:09 +0900

    [level 1] Title: 유연근무제, Time: 0.33 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,38 +1,18 @@
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int exce = 7 - startday;
         int result = 0;
         
         for(int i = 0; i < schedules.length; i++){
-            int curSchedules = schedules[i];
-            
-            int h = curSchedules/100; 
-            int m = curSchedules%100;
-            
-            if(m+10 >=60) {
-                h++;
-                m = m-60;
-            }
-            
-            int limitTime = h*100 + m + 10;
-            
-            boolean isEligible = true;
-            
             for(int j = 0; j < timelogs[i].length; j++){
-                int exce = (startday + j -1) % 7 + 1;
-                if(exce == 6 || exce == 7) {
+                if(exce-1 == j || exce == j) {
                     continue;
                 }
-                if(timelogs[i][j] > limitTime) {
-                    isEligible = false;
-                    break;
+                if(timelogs[i][j] == schedules[i] || timelogs[i][j] <= schedules[i]+10) {
+                    result++;
                 }
             }
-            
-            if (isEligible) {
-                result++;
-            }
         }
-        
             
         
         return result;
