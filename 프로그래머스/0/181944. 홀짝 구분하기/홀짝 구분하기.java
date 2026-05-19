f7b6d0fb371194fa7ca4969075b6071b429b340c
김태남 <63178138+kim-tea-nam@users.noreply.github.com>
2026-05-19 23:37:19 +0900

    [level 0] Title: 홀짝 구분하기, Time: 98.81 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n%2 == 0){
+            System.out.println(n+" is even");
+        } else {
+            System.out.println(n+" is odd");
+        }
+        
+        
+    }
+}
